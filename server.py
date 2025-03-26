d632472f0efb5549f50c96ec4dbb2139f507751b
SHUBHANSHU MISHRA <rollsroyace360@gmail.com>
2025-03-26 19:35:56 +0530

    port update

--- server.py
+++ server.py
@@ -11,3 +11,9 @@ def calculate_bmi(weight_kg: float, height_m: float) -> float:
 
 
 # Run the MCP server
+
+if __name__ == "__main__":
+    mcp.run(host='0.0.0.0', port=8000)
+
+
+# The server will be running on http://localhost:8080/
